21996ac33e6c70984c039f30920431096c53a982
mlehau21 <mle.hau21@gmail.com>
2023-03-08 19:30:01 +0700

    khoi tao

---
+++ lib/calculator/bmr.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../calculator/navigation_bar.dart';
+
+class BMRScreen extends StatefulWidget {
+  @override
+  _BMRScreenState createState() => _BMRScreenState();
+}
+
+class _BMRScreenState extends State<BMRScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMR Calculator'),
+      ),
+      body: Center(
+        child: Text('BMR Screen'),
+      ),
+    );
+  }
+}
